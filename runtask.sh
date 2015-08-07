ba3a3981ecbdd4d296d2bb2b2095dc501a968ac5
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-08-07 16:35:42 +0200

    get output

--- runtask.sh
+++ runtask.sh
@@ -6,6 +6,7 @@ STATE=
 
 function registerTask() {
 	aws ecs register-task-definition --cli-input-json "$(<$1.json)"
+	
 }
 
 function runTask() {
@@ -24,8 +25,18 @@ function printSummary() {
 	echo "$STATE" | jq -r '.tasks[0].containers[0] | .lastStatus ,  .reason , .exitCode'
 }
 
-registerTask $TASK
+function getInstanceId() {
+	CONTAINER_INSTANCE=$(echo "$STATE" | jq -r '.tasks[0].containerInstanceArn')
+	aws ecs describe-container-instances --cluster $CLUSTER --container-instances  $CONTAINER_INSTANCE  | jq -r '.containerInstances[0].ec2InstanceId'
+}
+
+function getInstanceIp() {
+	aws ec2 describe-instances --instance-id $(getInstanceId) | jq -r '.Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicIp'
+}
+
+registerTask $TASK > /dev/null
 ID=$(runTask $CLUSTER $TASK)
+echo $ID 
 getState $CLUSTER $ID
 while [ "$(getStatus)" == PENDING ] ; do
 	printSummary
@@ -33,3 +44,8 @@ while [ "$(getStatus)" == PENDING ] ; do
 	getState $CLUSTER $ID
 done
 printSummary
+
+IP=$(getInstanceIp)
+ssh ec2-user@$IP <<!
+docker logs \$(docker ps -a | grep mvanholsteijn/ecs-bug | head -1 | awk '{print \$1}')
+!
